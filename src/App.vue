390fec21e0f5012d3212e2c444fb0315cdac18a5
michihiko_karino <michihiko.karino@gmail.com>
2022-08-31 01:39:04 +0900

    vite vue-tscのボイラプレートコードから不要ファイル削除
    - cssプリプロセッサとしてsassを採用
    - reset.cssの採用

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  setup() {
+    const TITLE = 'フロントエンドコーディング試験';
+
+    return { TITLE };
+  }
+});
+
+</script>
+
+<template>
+  <h1>{{ TITLE }}</h1>
+</template>
+
+<style scoped>
+
+</style>
